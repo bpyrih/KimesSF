44b1977cf7c7e01a3eb1ef8d7e138a13182b8d4c
Andrii Makarchuk <andrii.makarchuk4@gmail.com>
2024-11-11 15:00:08 +0200

    added case to opportunity convert

--- force-app/main/default/lwc/lwcUtils/constantUtils.js
+++ force-app/main/default/lwc/lwcUtils/constantUtils.js
@@ -4,7 +4,9 @@ class ConstantUtils {
 
     CASE_CONVERSION_MESSAGES = {
         SUCCESS_MESSAGE : 'Case converted to lead successfully!',
+        SUCCESS_MESSAGE_OPP : 'Case converted to opportunity successfully!',
         WEB_FIELDS_ERROR_MESSAGE : 'Case could not be converted to lead. Missed fields on Case: {0}!',
+        WEB_FIELDS_ERROR_MESSAGE_OPP : 'Case could not be converted to opportunity. Missed fields on Case: {0}!',
         PROJECT_DATA_MISSED_ERROR_MESSAGE : '"Project Name" or "Project Address" should be populate!',
         FATAL_ERROR_MESSAGE : 'Fatal Error! Please contact the administrator'
     };
